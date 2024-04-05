2a147bd0ce52e85f678d7532f45203844c5dfec7
Nathan Beddoe <nbedd2@protonmail.com>
2024-04-05 13:18:22 +1100

    rename API

--- src/test/queryDb.ts
+++ src/test/queryDb.ts
@@ -1,12 +1,12 @@
-import client from '../client'
+import client from "../client";
 
 const d1Client = client({
-    accountId: process.env.CLOUDFLARE_ACCOUNT_ID ?? "",
-    apiKey: process.env.CLOUDFLARE_API_KEY ?? "",
-    apiEmail: process.env.CLOUDFLARE_API_EMAIL ?? "",
-    databaseId: process.env.CLOUDFLARE_DATABASE_ID ?? ""
-})
+  accountId: process.env.CLOUDFLARE_ACCOUNT_ID ?? "",
+  apiKey: process.env.CLOUDFLARE_API_KEY ?? "",
+  apiEmail: process.env.CLOUDFLARE_API_EMAIL ?? "",
+  databaseId: process.env.CLOUDFLARE_DATABASE_ID ?? "",
+});
 
-d1Client.queryDatabase("SELECT * FROM textEntries").then((data) => {
-    console.log("Query complete", data)
-})
+d1Client.query("SELECT * FROM textEntries").then((data) => {
+  console.log("Query complete", data);
+});
